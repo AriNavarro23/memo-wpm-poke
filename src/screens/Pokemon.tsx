9f131eb2704502b0c09f119f0f95c29ccdc6cf82
Ariel Navarro <navarro.a.ariel@hotmail.com>
2023-03-23 16:17:10 -0300

    first

--- src/screens/Pokemon.tsx
+++ src/screens/Pokemon.tsx
@@ -1,4 +1,70 @@
+    import { useState } from "react";
+
+const POKEMONS = [
+    'Bulbasaur',
+    'Ivysaur',
+    'Venusaur',
+    'Charmander',
+    'Charmeleon',
+    'Charizard',
+    'Squirtle',
+    'Wartortle',
+    'Blastoise',
+    'Caterpie',
+    'Metapod',
+    'Butterfree',
+    'Weedle',
+    'Kakuna',
+    'Beedrill',
+    'Pidgey',
+    'Pidgeotto',
+    'Pidgeot',
+    'Rattata',
+    'Raticate',
+    'Spearow',
+    'Fearow',
+    'Ekans',
+    'Arbok',
+    'Pikachu',
+    'Raichu',
+    'Sandshrew',
+    'Sandslash'
+]
+
+const MATCH = Math.floor(Math.random() * POKEMONS.length);
+
+type Form = HTMLFormElement & {
+    pokemon: HTMLInputElement;
+}
+
 export default function Pokemon() {
-    return <div>{`<Pokemon />`}</div>;
-    
+    //para saber si gano o no, ver que almacena
+    const [ hasWon, toggleWon ] = useState(false);
+
+    function handleSubmit(event:React.FormEvent<Form>) {
+        event.preventDefault();
+
+        const { pokemon } = event.currentTarget;
+
+        if( pokemon.value.toLowerCase() === POKEMONS[MATCH]) {
+            toggleWon(true);
+        }
+    }
+
+    return ( 
+        <div>
+            <img 
+            height={512}
+            width={512}
+            style={{imageRendering: 'pixelated', filter:'brightness(0) invert(1)'}}
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                MATCH + 1
+                }.png`}
+                />
+                <form action="">
+                    <input type="text" />
+                    <button type="submit">Submit</button>
+                </form>
+        </div>
+    );
 }
--- src/screens/WordsPerMinute.tsx
+++ src/screens/WordsPerMinute.tsx
@@ -1,32 +1,79 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
+const WORDS = [
+    'programmer',
+    'reactJS',
+    'docker',
+    'notebook',
+    'visualSC',
+    'develop',
+    'useState',
+    'useEffect',
+    'component',
+    'renderizar'
+];
 
 export default function WordsPerMinute() {
-
+    //para ir guardando las palabras escritas
     const [ characterCount, setCharacterCount ] = useState(0);
+    //para 
     const [ buffer, setBuffer ] = useState('');
     //que palabra necesito adivinar
     const [ word, setWord ] = useState(() => WORDS[(Math.random() * WordsPerMinute.length) | 0 ]);
+    //para usarlo con el contador de tiempo
+    const [ time, setTime ] = useState(0);
 
-    function handleSubmit(event: React.FormEvent<HTMLFormElement>)
-    event?.preventDefault();
 
-    if (buffer === word){
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>){
+            event.preventDefault();
+    
 
+            if (buffer === word){
+                //para obtener una palabra random
+                setWord(WORDS[(Math.random() * WORDS.length) | 0]);
+                //para ir sumando las letras e ir contando
+                setCharacterCount((characterCount) => characterCount + word.length);
+            }
+            //para limpiar
+        setBuffer('');
     }
+        //useEffect para escuchar el setTime, crear un timeOut, reduce de a 1" y lo reinicia
+        //para que siga restando de a uno
+        useEffect(() => {
+            if ( time != 0 ) {
+                const timeout = setTimeout(() => setTime( time -1 ), 1000);
 
-    return <div style={{display: 'flex', flexDirection:'column', gap: 12, textAlign:'center'}}>
-        <h1 style={{fontSize:48}}>{word}</h1>
+                return () => clearTimeout(timeout);
+            }
+        }, [ time ]);
+
+        //funcion para resetear el tiempo y el contador de letras
+        function handlePlay () {
+            setTime(60);
+            setCharacterCount(0);
+        }
+
+
+    return ( 
+    <div style={{display: 'flex', flexDirection:'column', gap: 12, textAlign:'center'}}>
+        {Boolean(time) && <h1 style={{fontSize:48}}>{word}</h1>}
         <h2>Character typed: {characterCount}</h2>
-        <form onSubmit={handleSubmit}>
+        <h3>Remaining time: {time}</h3>
+        {time ? (
+            //le digo que me renderice el formulario con el input y el boton de submit
+            <form onSubmit={handleSubmit}>
             <input type="text" 
-            autoFocus
-            value={buffer}
-            onChange={(e) => setBuffer(e.target)}
-            />
+                    autoFocus
+                    value={buffer}
+                    onChange={(e) => setBuffer(e.target.value)}
+                    />
             <button type="submit">Submit</button>
         </form>
-        </div>;
-    
+        ) : (
+            //sino dame el boton play
+            <button onClick={handlePlay}>Play</button>
+        )}
+        </div>
+    );
 }
